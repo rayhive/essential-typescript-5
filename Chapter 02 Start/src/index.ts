6a096ff297f6e6dec076ce8ca55f622b900e7702
Reuben Amoako-Atta <yawatta@gmail.com>
2023-10-01 09:20:14 +0800

    Created start folder for chaper 2 examples

---
+++ Chapter 02 Start/src/index.ts
@@ -0,0 +1,16 @@
+import { TodoItem } from "./todoItem";
+import { TodoCollection } from "./todoCollection.js";
+ 
+let todos: TodoItem[] = [
+    new TodoItem(1, "Buy Flowers"), new TodoItem(2, "Get Shoes"),
+    new TodoItem(3, "Collect Tickets"), new TodoItem(4, "Call Joe", true)];
+ 
+let collection: TodoCollection = new TodoCollection("Adam", todos);
+ 
+console.clear();
+//console.log(`${collection.userName}'s Todo List`);
+console.log(`${collection.userName}'s Todo List ` 
+    + `(${ collection.getItemCounts().incomplete } items to do)`);  
+ 
+//collection.removeComplete();
+collection.getTodoItems(true).forEach(item => item.printDetails());
